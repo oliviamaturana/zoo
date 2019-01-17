239296c1eab314dbf0c9aedd303d8ef88418e6fa
Olivia Maturana <oliviamaturanamcca@students.berkeley.net>
2019-01-17 14:26:16 -0800

    1/17

--- zoo.js
+++ zoo.js
@@ -1,3 +1,22 @@
-function sleep(){
+function sleep(name){
+    console.log(name + " sleeps for 8 hours");
+
+}
+
+var favoriteFood = "bacon";
+
+function eat(name, food){
+    console.log(name + " eats " + food);
+    if(food == favoriteFood){
+        console.log("YUM!!! " + name + " wants more " + food);
+    }else{
+        sleep(name);
+    }
+}
+
+function run(){
+    sleep("Tigger");
+    eat("Tigger", "meat");
+    eat("Tigger", "bacon");
 
 }
