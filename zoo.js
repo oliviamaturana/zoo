45439d8884fc916a66d2f100281d3f4428c5eed4
Olivia Maturana <oliviamaturanamcca@students.berkeley.net>
2019-01-22 14:24:23 -0800

    1/17

--- zoo.js
+++ zoo.js
@@ -14,9 +14,54 @@ function eat(name, food){
     }
 }
 
+
+
 function run(){
     sleep("Tigger");
     eat("Tigger", "meat");
     eat("Tigger", "bacon");
 
 }
+
+class Polygon {
+
+    constructor(height, width) {
+        this.name = 'Polygon';
+        this.height = height;
+        this.width = width;
+    }
+
+    //method #1
+    sayName() {
+        console.log('Hi, I am a ', this.name + '.');
+    }
+
+    //method #2
+    sayHistory() {
+        console.log('"Polygon" is derived from the Greek polus (many) ' +
+            'and gonia (angle).');
+    }
+
+}
+
+
+
+class Tiger {
+
+    constructor(name, favoriteFood) {
+        // put your constructor content here
+        this.name = "Tiger";
+        this.favoriteFood =
+    }
+
+    sleep() {
+        console.log(this.name + " sleeps for 8 hours");
+    }
+
+    eat() {
+        // complete your eat function here!
+
+    }
+}
+
+
